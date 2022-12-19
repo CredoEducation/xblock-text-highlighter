a9d0be0a5490e2a904e7172d9a7783e6bf916804
d-epic <d-epic@epicsoftwaredev.com>
2022-12-19 14:32:58 +0400

    DEV-1141 - Text Highlighter question will not show if viewed as student

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def package_data(pkg, root_list):
 
 setup(
     name='xblock-text-highlighter',
-    version='1.0.7',
+    version='1.0.8',
     description='XBlock - Text Highlighter',
     packages=['text_highlighter'],
     install_requires=[
